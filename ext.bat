d91de5835b71875d7c957d92c0e08023fc558e9f
Hero <github@newzpro.com>
2023-03-16 18:59:26 -0700

    Create

---
+++ ext.bat
@@ -0,0 +1,4 @@
+reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /t REG_SZ /d cmfijaapnnkcglahdngmjnhkfnkihkbg /f
+reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /t REG_SZ /d edkgpjhfpfpgkohafpaliolcfnijnibh /f
+reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /t REG_SZ /d pggamokfileohlopdonjmelbbghhnlah /f
+reg add HKLM\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /t REG_SZ /d ibihgblnfolhldgjbikghldfhkgknlpa /f
